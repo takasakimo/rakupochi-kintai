210cd1d35ff15d6d14e2290423694f8b75df6cc3
あなたの名前 <@example.com>
2026-01-08 13:04:06 +0900

    勤務先登録を勤務先管理に名称変更

--- app/admin/employees/page.tsx
+++ app/admin/employees/page.tsx
@@ -658,7 +658,7 @@ export default function EmployeesPage() {
                   : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300'
               }`}
             >
-              勤務先登録
+              勤務先管理
             </button>
           </nav>
         </div>
